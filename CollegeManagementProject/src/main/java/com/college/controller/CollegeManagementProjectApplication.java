0d79d8bf7980e4384482095b8f2aa1fba32e2584
Durva <durvakulkarni3@gmail.com>
2022-07-05 22:51:15 +0530

    CollegeManagementProject

---
+++ CollegeManagementProject/src/main/java/com/college/controller/CollegeManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.college.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementProjectApplication.class, args);
+	}
+
+}
---
+++ CollegeManagementProject/src/main/java/com/college/repository/QueryRepository.java
@@ -0,0 +1,12 @@
+package com.college.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.college.dto.LoginData;
+
+@Repository
+public interface QueryRepository extends JpaRepository<LoginData, String>
+{
+	LoginData findByUsernameAndPasswordAndCategory(String username, String password, String category);
+}
