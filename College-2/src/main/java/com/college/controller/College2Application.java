1b3c6823052a6a4b8ee1ab8dbc98fbf038de72e3
Durva <durvakulkarni3@gmail.com>
2022-07-11 17:02:37 +0530

    Added

---
+++ College-2/src/main/java/com/college/controller/College2Application.java
@@ -0,0 +1,13 @@
+package com.college.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class College2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(College2Application.class, args);
+	}
+
+}
